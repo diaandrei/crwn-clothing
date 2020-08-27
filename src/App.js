5f9f27fe8973359681db4d95e74def1968a403c1
diaandrei <diamandescuandrei@gmail.com>
2020-08-27 14:38:31 +0300

    created the shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/Homepage/Homepage";
+import ShopPage from "./pages/Shop/Shop";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/Shop/Shop.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import CollectionPreview from "../../components/Collection-preview/Collection-preview";
+
+import SHOP_DATA from "./Shop.data.js";
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
